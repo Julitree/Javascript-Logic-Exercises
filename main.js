56eac75a2ae7e0f4048799c020df28587d9f8c64
Julia <julytree@hotmail.es>
2020-03-12 23:45:08 +0100

    Write using console.log the table from 9 to 9x10.
    Escribir utilizando console.log la tabla del 9 hasta 9x10.
    Write using console.log the table from 9 to 9x10.

---
+++ main.js
@@ -0,0 +1,6 @@
+// Escribir utilizando console.log la tabla del 9 hasta 9x10.
+//Write using console.log the table from 9 to 9x10.
+
+for(i=1; i < 10+1; i++){
+    console.log("Multiply " + i + " x 9 the result is " + i * 9  );
+}
