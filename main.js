d959746441ae05cad2bf98171999e73e6ac1ebbc
Julia <julytree@hotmail.es>
2020-03-14 12:07:47 +0100

    adding two numbers
    Write a program where the user  add a number in a prompt and then show another prompt
    asking another number and finally the program show you an alert telling you  the sum result
    
    Escribir un programa que muestre un prompt donde el usuario ingresa un número, 
       luego muestra otro prompt pidiendo otro número, finalmente el 
       programa muestra una alerta con el resultado de la suma.

---
+++ main.js
@@ -0,0 +1,12 @@
+
+/* Escribir un programa que muestre un prompt donde el usuario ingresa un número, 
+   luego muestra otro prompt pidiendo otro número, finalmente el 
+   programa muestra una alerta con el resultado de la suma.*/
+
+/* Write a program where the user add a number in a prompt and then show another prompt
+asking another number and finally the program show you an alert telling you  the sum result*/ 
+
+
+var num1 = prompt("Ingrese numero");
+var num2 = prompt("escriba otro numero");
+var resultado = alert(Number(num1) + Number(num2));
