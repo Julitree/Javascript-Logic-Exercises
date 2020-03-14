1be2b54d901b29f4d3736782c37a047bcda0b465
Julia <julytree@hotmail.es>
2020-03-14 12:17:01 +0100

    Divide number x 10
    Pedir un número a un usuario a través de un prompt y luego dividirlo por 10, 
    ejemplo: 5 / 10 igual 0.5 
    
    Asking number to the user from a prompt and then divide it to 10

---
+++ divideTwoNumbers/main.js
@@ -0,0 +1,9 @@
+/* Pedir un número a un usuario a través de un prompt y luego dividirlo por 10, 
+ejemplo: 5 / 10 igual 0.5 
+
+Asking number to the user from a promt and then divide it to 10*/
+
+var indicaNum = prompt("Write a number");
+var convertirANu = Number(indicaNum);
+var result = alert(convertirANu / 10);
+
